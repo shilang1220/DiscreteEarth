ca104021bec78e9fd8bf873b5bb1fe21ca638a1a
濮国梁 <pgl@pku.edu.cn>
2018-12-18 13:58:51 +0800

    add unit test support.

--- src/gt/gtcoords_test.cpp
+++ src/gt/gtcoords_test.cpp
@@ -4,10 +4,10 @@
 #include <gtest/gtest.h>
 #include "gt/gtcoords.h"
 
-TEST(CellID_TEST, ll){
+TEST(GTCOORDS_TEST, ll){
     double lng, lat;
     lng = 180.00;lat = 90.00;
     uint64 id;
-    GT::LLtoCellID(lng,lat,&id)
+    GT::LLtoCellID(lng,lat,&id);
     EXPECT_EQ(0X0,id);
 }
