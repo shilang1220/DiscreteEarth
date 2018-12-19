452d7da62f0327c69b405935c6bd68dae98eecdd
Guoliang PU <pgl@pku.edu.cn>
2018-12-19 16:09:50 +0800

    add GT_API dllexport support.

--- include/gt.h
+++ include/gt.h
@@ -5,4 +5,16 @@
 #ifndef DISCRETEEARTH_GT_H
 #define DISCRETEEARTH_GT_H
 
+//接口类库（core）
+#include "core/cap.h"
+#include "core/cell.h"
+#include "core/cell_id.h"
+#include "core/region.h"
+#include "core/latlng_rect.h"
+#include "core/latlng_rect_bounder.h"
+
+//S2支撑类库（s2）
+
+//GT基础类库（gt）
+
 #endif //DISCRETEEARTH_GT_H
--- test/gtcoords_test.cpp
+++ test/gtcoords_test.cpp
@@ -2,7 +2,7 @@
 // Created by Administrator on 2018/12/17 0017.
 //
 #include "gtest/gtest.h"
-#include "gt/gtcoords.h"
+#include "gt.h"
 
 TEST(GTCOORDS_TEST, ll){
     double lng, lat;
