348382018902f58f362245927037c1a2d38818a1
濮国梁 <pgl@pku.edu.cn>
2018-12-26 13:10:28 +0800

    Adjust gt_coords and gt_cell_id,to adapt to GT slice and coding scheme.

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # DiscreteEarth
 
-core -- classes for discrete cell and cell coding of sphere face.
-gt --  classes for spatial feature.
-s2 --  classes for definition,attribution,measure,projection and feature relation on sphere face.
-tools -- tools for debugging and logging.
+FOR USERS:
+1. add ${GTROOT} to your include directories.
+2. add gt.lib to your link libs.
+3. put gt.dll to your dll searching directories.
+4. add 'include "gt.h"' in your source code.
+5. using gt lib freely. 
+
+
+FOR DEVELOPERS:
+This lib composed by three main parts.
+PART I: 
+four thirdparty source code. Including:
+base/
+util/
+strings/
+thirdparty/
+
+PART II:
+classes for angles,arcs,3d points,latlng points,distances,areas etc. on sphere face. Including:
+s2/
+
+PART III:
+classes for GT libs, modified refer to S2Geometry. Including:
+core/	 	-- 	classes for discrete cell,cellid,cap,latlng_rect etc.
+gt/		 	--  classes for spatial feature. 
+index/		--	classes for spatial index in memory.
+query/      --  classes for spatial query in memory.
+tools/		-- 	tools for debugging and logging.
+
--- include/gt.h
+++ include/gt.h
@@ -24,22 +24,18 @@
 //GT核心类库（core）
 //以下为抽象类
 #include "core/region.h"                    //区域抽象类，各种球面区域类的抽象类，封装了球面区域的共性抽象接口
-// #include "tmp/cell_id.h"                 //网格单元编码抽象类，球面网格单元编码的抽象类，封装了球面网格单元编码的共性抽象接口
-// #include "tmp/cell.h"                    //网格单元抽象类，球面网格单元的抽象类，封装了球面网格单元的共性抽象接口
 #include "core/gt_shape.h"                  //Shape抽象类，点、线、环、面等shape的抽象类，封装了访问shape的共性抽象接口
-//以下为实体类
+//以下为核心类
 #include "core/gt_cap.h"                    //球冠类，用地心坐标系表达球面坐标范围，便于球面分析
 #include "core/gt_latlng_rect.h"            //经纬度矩形类，用经纬度坐标系表达球面范围，便于经纬度分析
 #include "core/gt_coords.h"                 //GT剖分方案中，各种坐标系之间的转换函数API
-#include "core/gt_cell.h"                   //GT剖分方案的网格单元类，对剖分网格单元进行建模，内部封装了基于剖分编码的各种网格单元操作，如：转码、单元属性、测量、关系计算等
 #include "core/gt_cell_id.h"                //GT剖分方案的网格单元编码类，对网格编码进行了封装
+#include "core/gt_cell.h"                   //GT剖分方案的网格单元类，对剖分网格单元进行建模，内部封装了基于剖分编码的各种网格单元操作，如：转码、单元属性、测量、关系计算等
 #include "core/gt_cell_union.h"             //GT剖分方案的网格集合类，对剖分网格单元集合进行建模，用于各种空间要素的空间范围网格化量化
 //#include "core/gt_metrics.h"              //常用指标计算类，暂时未使用，待完善
 
-
-
-
 //GT基础类库（gt）
+//所有空间要素都视为区域，GT实现了各类空间要素的网格化表达
 #include "gt/gt_point.h"                  //Point要素类，定义了由传统的点坐标模型向剖分网格模型转换的方法
 #include "gt/gt_multipoint.h"             //MultiPoint要素类，定义了由传统的多点坐标模型向剖分网格模型转换的方法
 #include "gt/gt_polyline.h"               //Polyline要素类，定义了由传统的线坐标串模型向剖分网格模型转换的方法
@@ -49,6 +45,8 @@
 //#include "gt/gt_region_cellunion_generator.h"       //点、线、环、面、球冠等区域类对象的剖分网格集合计算器，输入区域类对象和剖分参数，生成对应的剖分网格集合（ID集合）
 
 //GT索引类库（index）
+//实现了点数据索引（用于POI数据集等）、区域数据索引（用于图幅或要素的范围索引）和要素索引（索引到组成要素的边）三种形式
+//同时支持为外部数据组织管理系统生成地理编码
 #include "index/gt_point_index.h"             //点类型空间要素（索引对象为点数据，如POI点）的专用索引模型，（<cellid，PointData>的二叉树索引结构），用于POI点的邻近检索
 #include "index/gt_region_index.h"            //区域型空间要素的范围索引模型（索引对象为RegionLabel），（<cellid,RegionLabel>的顺序索引结构），通过对cellid的邻近或区域查询，获得与该cellid关联的空间要素Label集合
 #include "index/gt_shape_inex.h"              //点、线、面等空间要素索引模型，（<cellid, Edges>的map索引结构），每个cellid中关联的对象是空间要素的edges
